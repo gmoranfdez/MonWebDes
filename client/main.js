757714467d129bda584b8090b1efdf1906b71b53
gmoranfdez <gmoranfdez@gmail.com>
2016-12-13 14:16:26 +0100

    Login de google
    Login de google y nueva estructura para los layouts

--- client/main.js
+++ client/main.js
@@ -2,6 +2,15 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import './layouts/blog/blog_layout.html';
+import './layouts/contacto/contacto_layout.html';
+import './layouts/estadosgenerales/estadosgenerales_layout.html';
+import './layouts/home/home_layout.html';
+import './layouts/info/info_layout.html';
+//import './layouts/main/main_layout.html';
+//import './layouts/main/navigation_layout.hmtl';
+import './layouts/series/series_layout.html';
+
 
 Router.configure({
     layoutTemplate: 'main'
